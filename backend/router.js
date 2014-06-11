c3e5af2953317a6e8b20be0b2d90f923fe701d12
Josh Cunningham <josh.cunningham@10up.com>
2014-06-11 16:11:25 -0700

    Auth progress

--- backend/router.js
+++ backend/router.js
@@ -1,2 +1,30 @@
-/* Define REST api routes */
+/* globals module */
+
+module.exports = function (app, passport) {
+
+	/*
+	Front-end routes
+	*/
+
+	// Homepage
+	app.get('/', function (req, res) {
+		return res.render('home', {
+			title: 'the home page'
+		});
+	});
+
+	// Login page
+	app.get('/login', function (req, res) {
+		return res.render('login', {
+			title: 'the login page'
+		});
+	});
+
+	// Homepage
+	app.get('/register', function (req, res) {
+		return res.render('register', {
+			title: 'the login page'
+		});
+	});
+};
 
--- server.js
+++ server.js
@@ -1,26 +1,81 @@
-var express = require('express');
-var mongoose = require('mongoose');
+/* globals require, console, process */
 
+var
+	// Express server
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+
+	// DB
+	mongoose = require('mongoose'),
+
+	// Authentication
+	passport = require('passport'),
+	flash = require('connect-flash'),
+	session = require('express-session'),
+
+	// Server-side templates
+	expressHbs = require('express3-handlebars');
+
+// Fun starts here
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/dist'));
+
+// Log requests to the console
+app.use(function (req, res, next) {
+	console.log('%s %s', req.method, req.url);
+	next();
+});
+
+// Server-side templating
+app.engine('hbs', expressHbs({
+	extname: 'hbs',
+	defaultLayout: 'main.hbs'
+}));
+app.set('view engine', 'hbs');
+
+
+// Secrets for password and token hashing
+app.set('secret', process.env.SECRET || 'a691865436aaca1b7c5a755a57ea68db');
+app.set('jwtTokenSecret', process.env.JWT_SECRET || '95bf6e3620dce448f16d048f1d3854b7');
+
+// Read cookies for authentication
+// WTF: line below causes app to hang
+// app.use(cookieParser);
+
+// Parse POST requests to JSON
+app.use(bodyParser.json());
+
+// Authentication through Passport
+//app.use(session({ secret: app.get('secret') })); // session secret
+//app.use(passport.initialize());
+//app.use(passport.session());
+//app.use(flash);
 
 // connect to database
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/track-yourself';
 mongoose.connect(uristring, function (err, res) {
   if (err) {
-  console.log('ERROR connecting to: ' + uristring + '. ' + err);
+		console.log('ERROR connecting to: ' + uristring + '. \n' + err);
   } else {
-  console.log('Succeeded connected to: ' + uristring);
+		console.log('SUCCESS connecting to: ' + uristring);
   }
 });
 
-// start server
-var server = app.listen(app.get('port'), function() {
-  console.log('The server is running on ' + app.get('port'));
-});
+// Passport auth and JWT
+// var jwtauth = require('./lib/jwyAuth.js')(app);
+// require('./config/passport')(passport);
 
-server.on('error', function() {
-  console.log('Error; shutting down server.');
-  server.close();
-});
+require('./backend/router.js')(app, passport);
+
+// Let's get this party started
+app
+	.listen(app.get('port'), function() {
+		console.log('The server is running on ' + app.get('port'));
+	})
+	.on('error', function() {
+		console.log('ERROR: shutting down server...');
+		this.close();
+	});
 
