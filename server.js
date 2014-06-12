c8080abe028c3b12cf33d38c298e39431908911b
Aisa <aisapatino@gmail.com>
2014-06-12 09:19:03 -0700

    updated readme with project structure tweaks
    moved commented out code to bottom of server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
-
 var	passport = require('passport');
-var flash = require('connect-flash');
-var session = require('express-session');
-
 var expressHbs = require('express3-handlebars');
-
 
 // Initialize & configure express app
 var app = express();
@@ -36,21 +31,12 @@ app.use(bodyparser());
 app.set('secret', process.env.SECRET || 'a691865436aaca1b7c5a755a57ea68db');
 app.set('jwtTokenSecret', process.env.JWT_SECRET || '95bf6e3620dce448f16d048f1d3854b7');
 
-// Read cookies for authentication
-// WTF: line below causes app to hang
-// app.use(cookieParser);
-// Authentication through Passport
-//app.use(session({ secret: app.get('secret') })); // session secret
-//app.use(passport.initialize());
-//app.use(passport.session());
-//app.use(flash);
-
 // connect to database
 var dbLocations = {
   production: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/track-yourself',
+  development: 'mongodb://localhost/track-yourself',
   test: 'mongodb://localhost/track-yourself-test'
 };
-
 mongoose.connect(dbLocations[nodeEnv], function (err) {
   if (err) {
   console.log('ERROR connecting to: ' + dbLocations[nodeEnv] + '. ' + err);
@@ -59,21 +45,36 @@ mongoose.connect(dbLocations[nodeEnv], function (err) {
   }
 });
 
-// initialize routing
+// Initialize backend routing
 require('./backend/router.js')(app, passport);
 require('./backend/routes/sleep-router.js')(app);
+
+
+// Start server
+app.listen(app.get('port'), function() {
+  console.log('The server is running on ' + app.get('port'));
+})
+.on('error', function() {
+  console.log('ERROR: shutting down server.');
+  this.close();
+});
+
+
+/*
+var flash = require('connect-flash');
+var session = require('express-session');
+*/
 
 // Passport auth and JWT
 // var jwtauth = require('./lib/jwyAuth.js')(app);
 // require('./config/passport')(passport);
 
-
-// Let's get this party started
-app.listen(app.get('port'), function() {
-  console.log('The server is running on ' + app.get('port'));
-})
-.on('error', function() {
-  console.log('ERROR: shutting down server...');
-  this.close();
-});
+// Read cookies for authentication
+// WTF: line below causes app to hang
+// app.use(cookieParser);
+// Authentication through Passport
+//app.use(session({ secret: app.get('secret') })); // session secret
+//app.use(passport.initialize());
+//app.use(passport.session());
+//app.use(flash);
 
