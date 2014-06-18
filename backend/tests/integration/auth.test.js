2f00487e51e8fcdcda3e7cecb587a3fa86fcc474
Aisa <aisapatino@gmail.com>
2014-06-18 09:12:10 -0700

    outline tests

---
+++ backend/tests/integration/auth.test.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect;
+
+describe('Passport authorization', function() {
+
+  it('should return 401 when accessing /dashboard not logged in', function() {
+    
+  });
+
+  it('should be able to register', function() {
+    
+  });
+
+  it('should login after registering automatically', function() {
+    
+  });
+  
+
+  it('should be able to login', function() {
+    
+  });
+  
+  it('should access public static files without auth', function() {
+    
+  });
+  
+  it('should be required to login when trying to access base angular url', function() {
+    
+  });
+
+  it('should be able to access home, login, register without auth', function() {
+    
+  });
+  
+  
+  
+   
+
+  
+});
