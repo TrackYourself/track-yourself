1d505f5098e67896956704e3d492dbbf2c1a95ae
Chelsea Lura <mail@chelsealura.com>
2014-06-19 10:16:20 -0700

    Basic test

---
+++ app/tests/integration/basic.test.js
@@ -0,0 +1,14 @@
+var chai=require('chai');
+var chaiAsPromised=require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect=chai.expect;
+
+describe("hello-protractor", function () {
+  describe("index", function () {
+    it("should display the correct title", function () {
+      // in the video, I used the protractor.getInstance() which was removed shortly thereafter in favor of this browser approach
+      browser.get('/#');
+      expect(browser.getTitle()).to.equal('TRACK YOSELF');
+    });
+  });
+});
