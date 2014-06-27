bd20fd16a14f47a5fbf604ddc774e72fdc5db32e
Chelsea Lura <mail@chelsealura.com>
2014-06-27 13:00:18 -0700

    Edited test-data; fixed protractor exercise test; added label for notes on exercise form

--- app/tests/integration/exercise.test.js
+++ app/tests/integration/exercise.test.js
@@ -1,6 +1,5 @@
-/* jshint expr:true */
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-var expect = chai.expect;
-var utils = require('./../../../backend/tests/utils.js');
+// /* jshint expr:true */
+// var chai = require('chai');
+// var chaiAsPromised = require('chai-as-promised');
+// chai.use(chaiAsPromised);
+// var expect = chai.expect;
